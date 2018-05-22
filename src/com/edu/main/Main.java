be5690457e71293f7ece814d6f5efbd05e577c98
AmanDev <aman.yahway@gmail.com>
2018-05-22 11:43:53 -0500

    modified

--- src/com/edu/main/Main.java
+++ src/com/edu/main/Main.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 import com.edu.domain.Order;
@@ -13,6 +14,7 @@ import com.edu.emailserviceImpl.EmailServiceImpl;
 import com.edu.service.OrderService;
 
 @Component
+@EnableScheduling
 public class Main {
 	
 	@Autowired
