c9c53b99111602861089b219fb763b93944bcc39
AmanDev <aman.yahway@gmail.com>
2018-05-22 16:04:39 -0500

    Junit added

---
+++ test/com/edu/domain/OrderServiceTest.java
@@ -0,0 +1,30 @@
+package com.edu.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.edu.service.OrderService;
+import com.edu.service.impl.OrderServiceImpl;
+
+public class OrderServiceTest {
+	@Autowired
+	OrderService os = new OrderServiceImpl();
+	
+	@Before
+	public void saveOrder(){
+		Order od  = new Order();
+		od.setName("pen");
+		od.setId(1);
+		os.save(od);
+	}
+	
+	
+	@Test
+    public void getOrder(){
+		Order test = os.find((long) 1);
+        assertEquals(test.getName(), "pen");
+    }
+}
