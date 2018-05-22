26257420b411e3c07b416a26dc609a56f2a52d93
AmanDev <aman.yahway@gmail.com>
2018-05-21 22:36:19 -0500

    Refactored

--- src/com/edu/main/Main.java
+++ src/com/edu/main/Main.java
@@ -9,8 +9,7 @@ public class Main {
 public static void main(String[] args) {
 	ApplicationContext ctx = new ClassPathXmlApplicationContext("context/applicationContext.xml");
 	Order od = (Order)ctx.getBean("testOrder");
-	
- System.out.println(od.getName());
+   System.out.println(od.getName());
 	
 }
 }
